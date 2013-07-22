fb4af70768f72808098d8ad5199d5561dcdc6b5d
Stefan Schneider <stefan.schneider@mni.thm.de>
2013-07-22 14:34:37 +0200

    Added submenu

--- com_thm_repo/admin/controller.php
+++ com_thm_repo/admin/controller.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * @package  	com_thm_repo
- * @author      Stefan Schneider	<stefan.schneider@mni.thm.de>
+ * @category    Joomla component
+ * @package	    THM_Repo
+ * @subpackage  com_thm_repo.admin
+ * @author      Stefan Schneider, <stefan.schneider@mni.thm.de>
  * @copyright   2013 TH Mittelhessen
  * @license     GNU GPL v.2
  * @link        www.mni.thm.de
@@ -12,22 +14,39 @@
 defined('_JEXEC') or die;
 
 /**
- * General Controller of THM Repo Component
+ * THM_RepoController class for component com_thm_repo
+ *
+ * @category  Joomla.Component.Admin
+ * @package   com_thm_repo.admin
+ * @link      www.mni.thm.de
+ * @since     Class available since Release 2.0
  */
 class THM_RepoController extends JController
 {
 	/**
-	 * display task
+	 * Method to display admincenter
+	 * 
+	 * @param   boolean  $cachable   cachable
+	 * @param   boolean  $urlparams  url param
 	 *
 	 * @return void
 	 */
-	function display($cachable = false, $urlparams = false)
+	public function display($cachable = false, $urlparams = false)
 	{
-		// set default view if not set
+		// Set default view if not set
 		$input = JFactory::getApplication()->input;
 		$input->set('view', $input->getCmd('view', 'folders'));
+		
+		
+		// Submenu
+		$vName = JRequest::getWord('view', 'thm_repo');
+		
+		JSubMenuHelper::addEntry(JText::_('COM_THM_REPO_FOLDERMANAGER'), 'index.php?option=com_thm_repo&view=folders', $vName == 'folders');
+		JSubMenuHelper::addEntry(JText::_('COM_THM_REPO_FILEMANAGER'),  'index.php?option=com_thm_repo&view=files', $vName == 'files');
+		JSubMenuHelper::addEntry(JText::_('COM_THM_REPO_LINKMANAGER'), 'index.php?option=com_thm_repo&view=links', $vName == 'links');
 	
-		// call parent behavior
+		// Call parent behavior
 		parent::display($cachable);
 	}
 }
+
