6506135d7c814c2b3e89cee141e192eca9574d3f
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2013-07-23 14:37:12 +0200

    change to nested trees

--- com_thm_repo/admin/sql/install.sql
+++ com_thm_repo/admin/sql/install.sql
@@ -1,63 +1,71 @@
+START TRANSACTION;
+
 CREATE TABLE #__thm_repo_folder (
 	id int(10) UNSIGNED AUTO_INCREMENT,
+	lft int(12) UNSIGNED NULL,
+	rgt int(12) UNSIGNED NULL,
 	parent_id int(10) UNSIGNED NULL,
 	name varchar(25) NOT NULL,
-	description varchar(255),
-	created Timestamp,
-	modified Timestamp,
+	description varchar(255) NULL,
+	created Timestamp NOT NULL,
+	modified Timestamp NOT NULL,
 	modified_by int(11) NOT NULL,
-	create_by int(11) NOT NULL,
-	viewlevels int(10) UNSIGNED NOT NULL,
+	created_by int(11) NOT NULL,
+	viewlevel int(10) UNSIGNED NOT NULL,
 	PRIMARY KEY(id),
+	KEY thm_repo_folder_lft (lft),
+	KEY thm_repo_folder_rgt (rgt),
 	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT,
 	FOREIGN KEY(modified_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-	FOREIGN KEY(create_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-	FOREIGN KEY(viewlevels) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE #__thm_repo_entity (
-	id int(10) UNSIGNED AUTO_INCREMENT,
-	parent_id int(10) UNSIGNED NOT NULL,
-	name varchar(25) NOT NULL,
-	description varchar(255),
-	created Timestamp,
-	modified Timestamp,
-	modified_by int(11) NOT NULL,
-	create_by int(11) NOT NULL,
-	viewlevels int(10) UNSIGNED NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-	FOREIGN KEY(modified_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-	FOREIGN KEY(create_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
-	FOREIGN KEY(viewlevels) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
-
+	FOREIGN KEY(created_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(viewlevel) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE #__thm_repo_file (
 	id int(10) UNSIGNED,
-	path varchar(100) NOT NULL,
-	size long,
-	mimetype varchar(100),
+	parent_id int(10) UNSIGNED NOT NULL,
+	current_version int(10) UNSIGNED NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(id) REFERENCES #__thm_repo_entity(id) ON UPDATE CASCADE ON DELETE CASCADE
-
+	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT
+ 	-- , FOREIGN KEY(current_version) REFERENCES #__thm_repo_version(version) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE #__thm_repo_version (
-	id int(10) UNSIGNED NOT NULL,
-	versionnumber int(10) UNSIGNED NOT NULL,
-	name varchar(25) NOT NULL, 
-	modified timestamp,
+	id int(10) UNSIGNED AUTO_INCREMENT,
+	version int (10) UNSIGNED NOT NULL DEFAULT 1,
+	name varchar(25) NOT NULL,
+	description varchar(255),
+	created Timestamp,
+	modified Timestamp,
+	modified_by int(11) NOT NULL,
+	created_by int(11) NOT NULL,
+	viewlevel int(10) UNSIGNED NOT NULL,
 	path varchar(100) NOT NULL,
 	size long NOT NULL,
-	mimetype varchar(20) NOT NULL,
-	PRIMARY KEY(id, versionnumber),
-	FOREIGN KEY(id) REFERENCES #__thm_repo_file(id) ON UPDATE CASCADE ON DELETE CASCADE
+	mimetype varchar(100) NOT NULL,
+	PRIMARY KEY(id, version),
+	FOREIGN KEY(id) REFERENCES #__thm_repo_file(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(modified_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(created_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(viewlevel) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE #__thm_repo_link (
-	id int(10) UNSIGNED ,
+	id int(10) UNSIGNED AUTO_INCREMENT,
+	parent_id int(10) UNSIGNED NOT NULL,
+	name varchar(25) NOT NULL,
+	description varchar(255),
+	created Timestamp NOT NULL,
+	modified Timestamp NOT NULL,
+	modified_by int(11) NOT NULL,
+	created_by int(11) NOT NULL,
+	viewlevel int(10) UNSIGNED NOT NULL,
 	link varchar(250) NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(id) REFERENCES #__thm_repo_entity(id)
+	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(modified_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(created_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(viewlevel) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+COMMIT;
--- com_thm_repo/admin/sql/uninstall.sql
+++ com_thm_repo/admin/sql/uninstall.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS #__thm_repo_link;
 DROP TABLE IF EXISTS #__thm_repo_version;
 DROP TABLE IF EXISTS #__thm_repo_file;
-DROP TABLE IF EXISTS #__thm_repo_entity;
 DROP TABLE IF EXISTS #__thm_repo_folder;
