b5ff5a97521301756ded3283984e3c6da43b33ee
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2013-07-23 15:44:54 +0200

    add entity again to have a ordering for links and files

--- com_thm_repo/admin/sql/install.sql
+++ com_thm_repo/admin/sql/install.sql
@@ -7,8 +7,8 @@ CREATE TABLE #__thm_repo_folder (
 	parent_id int(10) UNSIGNED NULL,
 	name varchar(25) NOT NULL,
 	description varchar(255) NULL,
-	created Timestamp NOT NULL,
-	modified Timestamp NOT NULL,
+	created timestamp NOT NULL,
+	modified timestamp NOT NULL,
 	modified_by int(11) NOT NULL,
 	created_by int(11) NOT NULL,
 	viewlevel int(10) UNSIGNED NOT NULL,
@@ -21,12 +21,19 @@ CREATE TABLE #__thm_repo_folder (
 	FOREIGN KEY(viewlevel) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE #__thm_repo_file (
-	id int(10) UNSIGNED,
+CREATE TABLE #__thm_repo_entity (
+	id int (10) UNSIGNED,
 	parent_id int(10) UNSIGNED NOT NULL,
-	current_version int(10) UNSIGNED NOT NULL,
+	ordering int(12),
 	PRIMARY KEY(id),
 	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE #__thm_repo_file (
+	id int(10) UNSIGNED,
+	current_version int(10) UNSIGNED NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(id) REFERENCES #__thm_repo_entity(id) ON UPDATE CASCADE ON DELETE CASCADE
  	-- , FOREIGN KEY(current_version) REFERENCES #__thm_repo_version(version) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -35,8 +42,8 @@ CREATE TABLE #__thm_repo_version (
 	version int (10) UNSIGNED NOT NULL DEFAULT 1,
 	name varchar(25) NOT NULL,
 	description varchar(255),
-	created Timestamp,
-	modified Timestamp,
+	created timestamp,
+	modified timestamp,
 	modified_by int(11) NOT NULL,
 	created_by int(11) NOT NULL,
 	viewlevel int(10) UNSIGNED NOT NULL,
@@ -52,7 +59,6 @@ CREATE TABLE #__thm_repo_version (
 
 CREATE TABLE #__thm_repo_link (
 	id int(10) UNSIGNED AUTO_INCREMENT,
-	parent_id int(10) UNSIGNED NOT NULL,
 	name varchar(25) NOT NULL,
 	description varchar(255),
 	created Timestamp NOT NULL,
@@ -62,7 +68,7 @@ CREATE TABLE #__thm_repo_link (
 	viewlevel int(10) UNSIGNED NOT NULL,
 	link varchar(250) NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(parent_id) REFERENCES #__thm_repo_folder(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY(id) REFERENCES #__thm_repo_entity(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY(modified_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
 	FOREIGN KEY(created_by) REFERENCES #__users(id) ON UPDATE CASCADE ON DELETE RESTRICT,
 	FOREIGN KEY(viewlevel) REFERENCES #__viewlevels(id) ON UPDATE CASCADE ON DELETE RESTRICT
--- com_thm_repo/admin/sql/uninstall.sql
+++ com_thm_repo/admin/sql/uninstall.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS #__thm_repo_link;
 DROP TABLE IF EXISTS #__thm_repo_version;
 DROP TABLE IF EXISTS #__thm_repo_file;
+DROP TABLE IF EXISTS #__thm_repo_entity;
 DROP TABLE IF EXISTS #__thm_repo_folder;
