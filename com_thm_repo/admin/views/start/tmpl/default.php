01417ab91e5a4d3a2bf651706b605e967006786c
Jeroen Klein <jeroen.klein@mni.thm.de>
2014-11-24 19:14:08 +0100

    Export: New icon for the export button.

--- com_thm_repo/admin/views/start/tmpl/default.php
+++ com_thm_repo/admin/views/start/tmpl/default.php
@@ -53,7 +53,7 @@ $document->addStyleSheet("components/com_thm_repo/css/start/start.css");
     <div class="menuitem">
         <div class="icon" onclick="location.href='index.php?option=com_thm_repo&task=doExport';">
             <div class="picture2">
-                <img src="components/com_thm_repo/img/download.png" width="60" height="40" align="center" alt="Export" />
+                <img src="components/com_thm_repo/img/icon-48-export.png" align="center" alt="Export" />
             </div>
 
             <div class="description2"><?php echo JText::_('COM_THM_REPO_EXPORTMANAGER');?></div>
